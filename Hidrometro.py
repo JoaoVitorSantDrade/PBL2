3ab4c083c66af661a6e3c15bf0ad6dafa38ed056
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2022-10-09 17:01:27 -0300

    Update Hidrometro.py

--- Hidrometro.py
+++ Hidrometro.py
@@ -11,9 +11,12 @@ from time import localtime, strftime
 
 class Hidrometro:
     
-    def __init__(self,hidrante):   
+    def __init__(self,hidrante,host,port):   
         self.hidrante = hidrante
         self.clientMQTT = mqtt_client.Client(hidrante.id)
+
+    def HidrometroServerMQTT():
+        pass
     
     def HidrometroClient(self,host_to_connect,port_to_connect): #Envia os dados para o servidor (Nuvem)
         while True: 
@@ -24,7 +27,7 @@ class Hidrometro:
                 print("Hidrometro fechado - consumo não contabilizado")
 
             try:
-                mqtt_client.Client_Connect(self.clientMQTT,host_to_connect,port_to_connect)
+                mqtt_client.Client_Connect(self.clientMQTT,host_to_connect,port_to_connect) #Conecta ao broker
             except Exception as err:
                 print("Erro na conexão do hidrometro - " + err)
             print("Conexão finalzada!")
@@ -41,14 +44,6 @@ class Hidrometro:
 
             mqtt_client.Multiple(self.hidrante.id,host_to_connect,port_to_connect,msgs)
 
-
-            self.hidrante.vazamento_valor = Json["vazamento_valor"]
-            self.hidrante.consumo = Json["consumo"]
-            self.hidrante.vazao = Json["vazao"]
-            self.hidrante.vazamento = Json["vazamento"]
-            self.hidrante.fechado = Json["fechado"]
-            self.hidrante.delay = Json["delay"]
-
             print("Consumo Atual: %s | Vazão Atual: %s | Vazamento Atual: %s | Fechado: %s" % (str(self.hidrante.consumo),str(self.hidrante.vazao),str(self.hidrante.vazamento_valor),str(self.hidrante.fechado)))
             time.sleep(self.hidrante.delay)
 
@@ -64,6 +59,13 @@ def main():
     hidrometro = Hidrometro(hidro)
 
     print("O seu ID é: %s\n" % (str(hidro.id)))
+
+    hidrometro.clientMQTT.subscribe("nevoa_test/hidrometro/"+str(hidrometro.hidrante.id)+"/consumo")
+    hidrometro.clientMQTT.subscribe("nevoa_test/hidrometro/"+str(hidrometro.hidrante.id)+"/vazao")
+    hidrometro.clientMQTT.subscribe("nevoa_test/hidrometro/"+str(hidrometro.hidrante.id)+"/vazamento")
+    hidrometro.clientMQTT.subscribe("nevoa_test/hidrometro/"+str(hidrometro.hidrante.id)+"/fechado")
+    hidrometro.clientMQTT.subscribe("nevoa_test/hidrometro/"+str(hidrometro.hidrante.id)+"/vazamento_valor")
+    hidrometro.clientMQTT.subscribe("nevoa_test/hidrometro/"+str(hidrometro.hidrante.id)+"/delay")
 
     try:
         server_process = Process(target=hidrometro.HidrometroClient, args=(connect_host,connect_port,))
