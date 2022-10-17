e6c95badd950ff814f31393e77845255ff2f50e9
ALrEcompUefs <contaofcali@gmail.com>
2022-10-17 15:23:28 -0300

    Criando Nuvem

---
+++ Nuvem/Nuvem.py
@@ -0,0 +1,35 @@
+import paho.mqtt.client as mqtt
+import mycallbacks
+import paho.mqtt.publish as publish
+
+# Lista dos ids de nevoas conectadas
+lista_nevoas_conectadas = []
+
+# Quando uma nova conexão é feita com o brocker
+def on_connect_nevoa(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+
+# Quando é feita uma publicação no topico em que a Nuvem esta inscrita 
+# Define o topico e atualiza os dados no dicionario
+def on_message_nevoa(client,userdata,message,tmp=None):
+    id_client = client.id
+    topico = message.topic
+    msg = message.payload
+    # salvar valor no client especifico
+
+# Quando uma nova publicação é feita pela Nuvem no brocker
+def on_publish_nevoa(client,userdata,mid):
+    print(mid)
+
+# Cria instância de cliente para observar topico de plubicação de dados
+def create_default_client(id_nevoa):
+    client = mqtt.Client(client_id=str(id_nevoa))
+    #client.username_pw_set()
+    client.on_message = on_message_nevoa
+    #client.on_connect = mycallbacks.on_connect
+    #client.on_publish = mycallbacks.on_publish
+    return client
