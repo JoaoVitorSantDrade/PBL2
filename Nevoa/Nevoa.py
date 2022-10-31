6cc4d6991a46e115b81663f6e27b6046e3c29e52
ALrEcompUefs <contaofcali@gmail.com>
2022-10-31 15:25:58 -0300

    nevoa 50%

--- Nevoa/Nevoa.py
+++ Nevoa/Nevoa.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 from random import seed
+from threading import Thread
 from random import randint
 import time
 import os
@@ -15,9 +16,13 @@ lista_vazamento_clients = {}
 lista_fechado_clients = {}
 lista_vazamento_valor_clients = {}
 lista_delay_clients = {}
+#   Valores fixos para bloqueio dos Hidrometros
+media = 1.0
+limite_consumo = 1.0
 
 #cria objeto nevoa
-def nevoaCliente(id):
+def nevoaCliente():
+    id = randint(0,100)
     client = create_default_client(id)
     return client
 #------------------------------------------------------------------------------------------------
@@ -39,6 +44,10 @@ def on_connect_nuvem(client, userdata, flags, rc):
 def on_publish_nevoa(client,userdata,mid):
     print(mid)
 
+def on_publish(client,userdata,mid):
+    #print(mid)
+    print("publicado")
+
 #-------------------------------------------------------------------------------------------------
 #   Métodos para registro dos dados lidos no brocker 
 
@@ -55,29 +64,63 @@ def on_message_nevoa(client,userdata,message,tmp=None):
     id_client = str(subtopicos[1])
     # salvar valor no client especifico
     if topico == "hidrometro/"+id_client+"/consumo":
-        consumo = int(msg)
+        consumo = float(msg)
         lista_consumo_clients[id_client] = consumo
+        print(lista_consumo_clients[id_client])
     elif topico == "hidrometro/"+id_client+"/vazao":
         vazao =float(msg)
         lista_vazao_clients[id_client] = vazao
+        print(lista_vazao_clients[id_client])
     elif topico == "hidrometro/"+id_client+"/vazamento":
         vazamento = float(msg)
         lista_vazamento_clients[id_client] =vazamento
+        print(lista_vazamento_clients[id_client])
     elif topico == "hidrometro/"+id_client+"/fechado":
-        fechado = bool(msg)
+        fechado = int(msg)
         print(fechado)
         lista_fechado_clients[id_client] = fechado
+        print(lista_fechado_clients[id_client])
     elif topico == "hidrometro/"+id_client+"/vazamento_valor":
         vazamento_valor = float(msg)
         lista_vazamento_valor_clients[id_client] = vazamento_valor
+        print(lista_vazamento_valor_clients[id_client])
     elif topico == "hidrometro/"+id_client+"/delay":
         delay = float(msg)
         lista_vazamento_valor_clients[id_client] = delay
+        print(lista_vazamento_valor_clients[id_client])
 
 # Quando é feita uma publicação no topico de ids de hidrometros
 # Insere o id na Lista
 def on_message_nevoa_to_getClients(client, userdata, message,tmp=None):
     lista_clients_conectados.append(client.id)
+
+#   Quando é feita uma publicação na Nuvem para a Nevoa
+def on_message_nuvem(client,userdata,message,tmp=None):
+    #print(dir(client))
+    topico = message.topic
+    msg = message.payload
+    
+    subtopicos = topico.split('/')
+    print(subtopicos[1])
+    #id_client = str(subtopicos[1])
+
+    #determina a operacao
+    if topico == "nuvem/media":
+        # atualiza a média
+        nova_media = float(msg) 
+        media = nova_media
+        print("Nova média:"+str(media))
+        pass
+    elif topico == "nuvem/limite":
+        # atualiza valor maximo
+        limite = float(msg) 
+        limite_consumo = limite
+        print("novo limite de consumo:"+str(limite_consumo))
+        pass
+    elif topico == "nuvem/bloquear":
+        pass
+    elif topico == "nuvem/solitar_media":
+        pass
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -97,10 +140,9 @@ def sub_to_getValues(client):
     client.subscribe("hidrometro/#") # se inscreve no topico de publicações dos hidrometros
     return client
 
-def on_publish(client,userdata,mid):
-    #print(mid)
-    print("publicado")
-
+def sub_to_getValues_nuvem(client):
+    client.subscribe("nuvem/#") # se inscreve no topico de publicações dos hidrometros
+    return client
 #-------------------------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------------------------
@@ -124,11 +166,48 @@ def create_default_client(id_nevoa):
     client.on_publish = on_publish
     return client
 
+def create_cliente_nuvem(id_nevoa):
+    client = mqtt.Client(client_id=str(id_nevoa))
+    #client.username_pw_set()
+    client.on_message = on_message_nuvem
+    client.on_connect = on_connect
+    client.on_publish = on_publish
+    return client
 #-------------------------------------------------------------------------------------------------
-def main():
-    clientMQTT = nevoaCliente(randint(0,100))
 
-    print( int(clientMQTT._client_id))
+#-------------------------------------------------------------------------------------------------
+#   Métodos de Execução das funcionalidades
+
+#   Executa a operação de receber dados publicados na névoa e inserir nas listas
+def executar_Leitura(host_to_connect,port_to_connect):
+    clientMQTT = nevoaCliente()
+    try:
+        Client_Connect(clientMQTT,host_to_connect,port_to_connect) #Conecta ao broker
+    except Exception as err:
+        print("Erro na conexão do hidrometro - " + err)
+    print("Conexão finalzada!")
+    clientMQTT = sub_to_getValues(clientMQTT)
+    while True:
+        time.sleep(3)
+
+def executar_Publicacao(host_to_connect,port_to_connect):
+   pass
+
+def executar_Leitura_Nuvem(host_to_connect,port_to_connect):
+    clientMQTT = create_cliente_nuvem(id)
+    try:
+        Client_Connect(clientMQTT,host_to_connect,port_to_connect) #Conecta ao broker
+    except Exception as err:
+        print("Erro na conexão do hidrometro - " + err)
+    print("Conexão finalzada!")
+
+    clientMQTT = sub_to_getValues_nuvem(clientMQTT)
+    while True:
+        time.sleep(3)
+
+def main():
+    
+    #print( int(clientMQTT._client_id))
     connect_host = input("Digite o IP em que devemos nos conectar: ")
     connect_port = int(input("Digite a Porta em que devemos nos conectar: "))
 
@@ -136,13 +215,14 @@ def main():
 
     #----------------------------------------------------
     try:
-        Client_Connect(clientMQTT,connect_host,connect_port) #Conecta ao broker
-    except Exception as err:
-        print("Erro na conexão do hidrometro - " + err)
-    print("Conexão finalzada!")
-    clientMQTT = sub_to_getValues(clientMQTT)
-    while True:
-         time.sleep(3)
+        leitura = Thread(target=executar_Leitura , args=(connect_host,connect_port))
+        leitura_nuvem = Thread(target=executar_Leitura_Nuvem,args=(connect_host,connect_port))
+    except KeyboardInterrupt:
+        print("Fechando os processos")   
+    finally:
+        leitura.start()
+        leitura_nuvem.start()
+        print("Fechando o programa") 
 
 if __name__ == '__main__':
     main()
